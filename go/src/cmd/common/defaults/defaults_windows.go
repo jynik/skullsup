78373e341428c331b72741cc44ffaeec93a3953e
Jon Szymaniak <jon.szymaniak@gmail.com>
2017-11-19 20:50:07 -0600

    go/cmd: Fixed linux vs windows definitions

--- go/src/cmd/common/defaults/defaults_windows.go
+++ go/src/cmd/common/defaults/defaults_windows.go
@@ -2,6 +2,7 @@
 package defaults
 
 const (
+	DEVICE_NAME   = "hexdump" // No direct connection supported in Windows currently...
 	WRITER_CONFIG = "\\skullsup\\queue-writer.conf"
 	READER_CONFIG = "\\skullsup\\queue-reader.conf"
 	DUMMY_PREFIX  = "%LOCALAPPDATA%"
