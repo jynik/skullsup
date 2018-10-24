18bc69959defb98ffee525869f84297d05e06b1f
Jon Szymaniak <jon.szymaniak@gmail.com>
2018-10-24 17:46:01 -0500

    go/cmd: Set default interface to /dev/ttyUSB0 (Linux)

--- go/src/cmd/common/defaults/defaults_linux.go
+++ go/src/cmd/common/defaults/defaults_linux.go
@@ -2,7 +2,7 @@
 package defaults
 
 const (
-	DEVICE_NAME   = "/dev/ttyACM0"
+	DEVICE_NAME   = "/dev/ttyUSB0"
 	DEVICE_TYPE   = "uart"
 	WRITER_CONFIG = "/.config/skullsup/queue-writer.conf"
 	READER_CONFIG = "/.config/skullsup/queue-reader.conf"
